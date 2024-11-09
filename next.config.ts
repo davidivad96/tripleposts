5a7038fa4f72562700252fecd54308522d59513c
davidivad96 <david@strivelabs.io>
2024-11-09 17:04:45 +0100

    set server actions body size limit to 10MB

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "img.clerk.com" }],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: 10000000, // 10MB
+    },
+  },
 };
 
 export default nextConfig;
