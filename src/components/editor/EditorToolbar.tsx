7bf8995d108d059e9d70ce6d843f6e56e4398e98
davidivad96 <david@strivelabs.io>
2024-11-19 11:31:42 +0100

    not allowing gifs as image attachments

--- src/components/editor/EditorToolbar.tsx
+++ src/components/editor/EditorToolbar.tsx
@@ -15,7 +15,7 @@ const EditorToolbar: React.FC<EditorToolbarProps> = ({
   <div className="flex items-center gap-1">
     <input
       type="file"
-      accept="image/png, image/jpeg, image/webp, image/avif, image/gif"
+      accept="image/png, image/jpeg, image/webp, image/avif"
       onChange={onImageUpload}
       className="hidden"
       id="image-upload"
